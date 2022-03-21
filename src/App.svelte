e9d00c7a05d1e2892be8a57ad738fa6182adcd4c
Camellia <56206613+sialiss@users.noreply.github.com>
2022-03-21 18:43:01 +0300

    make some small changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	export let name: string;
+	// здесь экспорт переменных из ts, можно использовать экспортированные переменные в файле
+	// export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Camellia</h1>
+	<p>Welcome to my portfolio!</p>
 </main>
 
 <style>

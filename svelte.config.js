358d7a7764f79ba3b7386ec9b8f571f5b973582a
Camellia <56206613+sialiss@users.noreply.github.com>
2022-08-07 20:10:53 +0300

    Upgrade deploy

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,12 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-	prerender:{default:true},
 	kit: {
+		prerender: { default: true },
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs',
-			fallback: '200.html'
+			fallback: null
 		}),
 
 		// Override http methods in the Todo forms

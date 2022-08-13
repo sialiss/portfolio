585a441b4c585da28ee8b7ec07560f06181189d8
Camellia <camellia.sialiss@gmail.com>
2022-08-13 17:28:56 +0300

    Updates

---
+++ _app/immutable/chunks/gamedev-2bd08f13.js
@@ -0,0 +1,1 @@
+const e=[{id:"dice",name:"Oh no! The dice came to life!",preview:"/images/dice.png",link:"/project",stack:"Godot",iframe_link:"https://itch.io/embed-upload/6179916?color=333333",about:"text",github:"https://github.com/sialiss/GMTK-gamejam2022"}];export{e as p};
